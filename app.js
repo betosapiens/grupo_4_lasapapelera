7ef22ff6d9c38f44ef461e459edf35130ea479ab
betosapiens <betosapiens@gmail.com>
2022-10-05 17:47:06 -0300

    productDetail

--- app.js
+++ app.js
@@ -1,18 +1,16 @@
 const express = require('express');
-const path = require('path');
-
 const app = express();
+app.use(express.static('public'));
 
-app.listen(3000, () => {
 
-
-    console.log('servidor iniciado');
-    
+app.listen(3000, ()=>{
+    console.log('Servidor funcionando');
 });
 
-app.get('/', (req, res) => {
+app.get('/', (req,res)=>{
+    res.sendFile(__dirname + '/views/index.html');
+});
 
-
-    res.sendFile(path.join(__dirname,'./views/index.html'));
-
-})
+app.get('/productDetail', (req,res)=>{
+    res.sendFile(__dirname + '/views/productDetail.html');
+});
